aa0fa56dd6528df87312b9ea5ba91930955ebc4e
prasadjw <prasadjayashanka@gmail.com>
2013-09-13 23:33:35 +0530

    created first version

---
+++ AlgorithmSimulator/src/algorithmsimulator/AlgorithmSimulator.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algorithmsimulator;
+
+import GUI.Interface;
+
+/**
+ *
+ * @author PRASAD
+ */
+public class AlgorithmSimulator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Interface i = new Interface();
+        i.setVisible(true);
+        
+    }
+}
